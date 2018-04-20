e93db5d4c71c96dad3af1fe4a752b0fcb7e027db
Not-Morgan <masonanderson0@gmail.com>
2018-04-19 23:28:57 -0700

    students and service tables exist, index lists student numbers

--- test2/models.py
+++ test2/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+# run manage.py makemigrations test2 && manage.py migrate to add to db
+# manage.py shell then from Test2.models import some stuff
+# Students.objects .all() .filter(first='')
+
+class Student(models.Model):
+    first = models.CharField(max_length=30)
+    last = models.CharField(max_length=30)
+    student_num = models.PositiveIntegerField()
+    homeroom = models.CharField(max_length=2)
+
+
+class Service(models.Model):
+    Student = models.ForeignKey(Student, on_delete=models.CASCADE)
+    cat_herding = models.DecimalField(max_digits=5, decimal_places=1)
+    surgery = models.DecimalField(max_digits=5, decimal_places=1)
+    sleeping = models.DecimalField(max_digits=5, decimal_places=1)
--- test2/views.py
+++ test2/views.py
@@ -1,6 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Student
+
 
 def index(request):
-    return HttpResponse("Hello, world.")
+    students = Student.objects.all()
+    html = "<h1>students</h1>"
+    for s in students:
+        html += "<p>{0}</p>".format(s.student_num)
+    return HttpResponse(html)
 
