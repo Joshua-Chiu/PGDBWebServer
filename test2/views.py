320f0da037fca3ad307a29fbd565c65197c20964
Josh <pgry@null.net>
2018-04-20 17:01:04 -0700

    html on test2

--- test2/views.py
+++ test2/views.py
@@ -1,17 +1,30 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Student
+from django.template.loader import get_template
 
-
+'''
 def index(request):
     students = Student.objects.all()
     html = "<h1>Students</h1>"
     for s in students:
         html += "<p>{1} {2} #{0}</p>".format(s.student_num, s.first, s.last)
     return HttpResponse(html)
+'''
 
 def search(request, num):
     html = ""
     for s in filter(lambda x: x.student_num == num, Student.objects.all()):
         html += "<p>{0}</p>".format(s.first)
     return HttpResponse(html)
+
+
+def index(request):
+	template = get_template('test2/index.html')
+	context = Context({
+	'Student': 'Student'
+	})
+
+	return HttpResponse(template.render(context, request))
+
+
