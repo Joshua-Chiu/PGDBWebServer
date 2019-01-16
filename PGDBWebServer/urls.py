facf94e359cb1d7d9456f9949cc14f157f07de3b
Josh <josh@Joshs-MacBook-Pro.local>
2019-01-16 00:34:34 -0800

    added login

--- PGDBWebServer/urls.py
+++ PGDBWebServer/urls.py
@@ -18,10 +18,12 @@ from django.urls import path, include
 from . import views
 
 urlpatterns = [
-    path('', views.test2, name="test2_redirect"),
+    # path('', views.test2, name="test2_redirect"),
+    path('', views.accounts, name="login"),
     path('test2/', include('test2.urls')),
     path('test3/', include('test3.urls')),
     path('export/', include('export.urls')),
     path('admin/', admin.site.urls),
+    path('accounts/', include('django.contrib.auth.urls')),
 ]
 
--- PGDBWebServer/views.py
+++ PGDBWebServer/views.py
@@ -1,4 +1,12 @@
 from django.http import HttpResponse, HttpResponseRedirect
 
+
 def test2(request):
     return HttpResponseRedirect('/test2/')
+
+
+def accounts(request):
+    return HttpResponseRedirect('/accounts/login')
+
+
+
