e79511a7a7686e73d54eba3cdb076ef34281a743
Mason Anderson <masonanderson0@gmail.com>
2019-06-25 11:43:33 -0700

    printing filtering now works for awards and grade

--- export/views.py
+++ export/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseRedirect
 from django.template.loader import get_template
-
+from test2.models import Student
+from util.queryParse import parseQuery
 
 def index(request):
     template = get_template('export/index.html')
@@ -13,8 +14,17 @@ def index(request):
 
 def printing(request):
     template = get_template('export/print.html')
-    context = {
 
+    year = request.GET["year"]
+    grade = request.GET["grade"]
+    award = request.GET["cumulative"]
+
+    query = f"grade:{grade} award:{award}"
+
+    students = parseQuery(query)
+
+    context = {
+        'student_list': students,
     }
     return HttpResponse(template.render(context, request))
 
