702754db767812f165e56e232bbc2374f8438268
Josh <pgry@null.net>
2019-06-28 19:36:34 -0700

    fixed admin form for users

--- users/admin.py
+++ users/admin.py
@@ -1,7 +1,6 @@
 from django.contrib import admin
 from django.contrib.auth import get_user_model
 from django.contrib.auth.admin import UserAdmin
-
 from .forms import CustomUserCreationForm, CustomUserChangeForm
 from .models import CustomUser
 
@@ -10,7 +9,12 @@ class CustomUserAdmin(UserAdmin):
     add_form = CustomUserCreationForm
     form = CustomUserChangeForm
     model = CustomUser
-    list_display = ['email', 'username', 'first_name', 'last_name', 'is_staff']
+    fieldsets = (
+        ('', {'fields': ('username',)}),
+        ('Personal Information', {'fields': ('first_name', 'last_name', 'email',)}),
+        ('Permissions', {'fields': ('is_active', 'is_superuser', 'is_staff', 'groups')}),
+        ('Dates', {'fields': ('last_login', 'date_joined')})
+    )
 
 
 admin.site.register(CustomUser, CustomUserAdmin)
