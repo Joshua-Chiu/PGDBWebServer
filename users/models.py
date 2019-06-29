8ab106545153c9c387d58a3f5646bc3afaf64148
Josh <pgry@null.net>
2019-06-29 00:54:53 -0700

    create autofocus field

--- users/models.py
+++ users/models.py
@@ -3,8 +3,7 @@ from django.db import models
 
 
 class CustomUser(AbstractUser):
-    pass
-    # add additional fields in here
+    autofocus = models.IntegerField(default=1, choices=[(1, 'Service'), (2, 'Athletics'), (3, 'Fine Arts'), (4, 'Scholar T1'), (5, 'Scholar T2')])
 
     def __str__(self):
         return self.first_name + " " + self.last_name
