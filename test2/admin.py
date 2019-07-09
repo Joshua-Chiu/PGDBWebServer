7ceb09ee467f0257cf16650cea8ed370582a56aa
Mason Anderson <masonanderson0@gmail.com>
2019-07-09 13:56:46 -0700

    export works import missing fields

--- test2/admin.py
+++ test2/admin.py
@@ -4,6 +4,7 @@ from .models import Student, PlistCutoff
 from import_export.admin import ImportExportModelAdmin
 from django.dispatch import receiver
 from import_export.signals import post_import, post_export
+from import_export.formats import base_formats
 # admin.site.register(Student)
 admin.site.register(PlistCutoff)
 
@@ -11,12 +12,32 @@ admin.site.register(PlistCutoff)
 class StudentResource(resources.ModelResource):
     class Meta:
         model = Student
-        import_id_fields = 'student_num'
-        exclude = 'id'
+        import_id_fields = ('student_num',)
+        fields = ('first', 'last', 'legal', 'student_num', 'homeroom', 'sex')
+        export_order = ['student_num', 'first', 'last', 'legal', 'sex']
 
 
 class StudentAdmin(ImportExportModelAdmin):
     resource_class = StudentResource
+
+    def get_export_formats(self):
+        formats = (
+            base_formats.CSV,
+            base_formats.TSV,
+            base_formats.XLSX,
+            base_formats.ODS,
+        )
+        return [f for f in formats if f().can_export()]
+
+
+    def get_import_formats(self):
+        formats = (
+            base_formats.CSV,
+            base_formats.TSV,
+            base_formats.XLSX,
+            base_formats.ODS,
+        )
+        return [f for f in formats if f().can_export()]
 
 
 admin.site.register(Student, StudentAdmin)
