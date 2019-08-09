8b8b658dc1f3ee649f32b1073427ddbcf6117aa5
Josh <pgry@null.net>
2019-08-08 22:35:33 -0700

    changed settings.py to use ssl

--- PGDBWebServer/settings.py
+++ PGDBWebServer/settings.py
@@ -156,7 +156,7 @@ AUTH_USER_MODEL = 'users.CustomUser'
 PROJECT_ROOT = os.path.join(os.path.abspath(__file__))
 STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
 
-# SECURE_SSL_REDIRECT = True, un comment for deployment
+SECURE_SSL_REDIRECT = True
 
 # Extra lookup directories for collectstatic to find static files
 STATICFILES_DIRS = (
