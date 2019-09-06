19c4c6c0ab867067c55b2ee7a056a84ccd28297f
Josh <pgry@null.net>
2019-09-06 15:44:55 -0700

    deleted heroku

--- PGDBWebServer/settings.py
+++ PGDBWebServer/settings.py
@@ -11,7 +11,6 @@ https://docs.djangoproject.com/en/2.0/ref/settings/
 """
 
 import os
-import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -169,6 +168,3 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 import dj_database_url
 prod_db = dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(prod_db)
-
-# Activate Django-Heroku.
-django_heroku.settings(locals())
