ffd9c0c9a12af82aab17bc664c458fee72a89d01
Josh <pgry@null.net>
2019-10-04 01:59:15 -0700

    we can export to excel

--- configuration/models.py
+++ configuration/models.py
@@ -4,8 +4,12 @@ from django.core.validators import MinValueValidator, MaxValueValidator
 
 
 class Configuration(models.Model):
-    principal_signature = models.ImageField(upload_to='export/uploads', default='export/uploads/no-img.png')
+    principal_signature = models.ImageField(upload_to='export/uploads', default='export/uploads/principal-signature.png')
     login_failure_limit = models.IntegerField(validators=[MinValueValidator(0), MaxValueValidator(20)], default=3)
+
+    se_help_text = models.CharField(max_length=50, verbose_name='Service Help Text', default='Service')
+    at_help_text = models.CharField(max_length=30, verbose_name='Athletics Help Text', default='Athletics')
+    fa_help_text = models.CharField(max_length=30, verbose_name='Fine Arts Help Text', default='Fine Arts')
 
     def save(self, *args, **kwargs):
         if not self.pk and Configuration.objects.exists():
