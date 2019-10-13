d57b76539d2b08510631644d773617a15b58c1b7
Josh <pgry@null.net>
2019-10-13 03:32:53 -0700

    threading for import pgdb; more ajax

--- data/ajax_views.py
+++ data/ajax_views.py
@@ -1,12 +1,14 @@
 from django.http import HttpResponse, HttpResponseRedirect, Http404, JsonResponse
 import os
 import datetime
-from .models import Student
-
+from .models import Student, PointCodes
+import xml.etree.ElementTree as ET
 import dateutil.parser
 import httplib2
 from googleapiclient.discovery import build
 from oauth2client.service_account import ServiceAccountCredentials
+
+logs = []
 
 
 def google_calendar():
@@ -41,13 +43,6 @@ def google_calendar():
             })
 
     return maintenance, notice
-
-
-def ajax_import_status(request):
-    data = {
-        'status': "10%",
-    }
-    return JsonResponse(data)
 
 
 def ajax_student_points_data(request):
@@ -112,3 +107,69 @@ def ajax_student_cumulative_data(request):
 
     return JsonResponse(data)
 
+
+def import_pgdb_file(tree):
+    global logs
+    root = tree.getroot()
+    # all students
+    for s in root[0]:
+        try:
+            if len(Student.objects.filter(student_num=int(s[0].text))) != 0:
+                print(f"student with number {s[0].text} already exists")
+                logs.append(f"student with number {s[0].text} \t ({s[4].text}, {s[3].text}) already exists")
+                continue
+
+            s_obj = Student(
+                student_num=int(s[0].text),
+                homeroom=f"{s[1].text.zfill(2)}{s[2].text}",
+                first=s[3].text,
+                last=s[4].text,
+                legal=s[5].text,
+                sex=s[6].text,
+                grad_year=int(s[7].text)
+            )
+            s_obj.save()
+
+            for g in s[8]:
+
+                g_obj = s_obj.grade_set.get(grade=int(g[0].text))
+                g_obj.anecdote = g[2].text or ""
+
+                scholar = g_obj.scholar_set.all().first()
+
+                scholar.term1 = float(g[3].text)
+                scholar.term2 = float(g[4].text)
+                scholar.save()
+
+                g_obj.save()
+
+                for p in g[5]:  # fix so that the codes are the last 2 digits instead of last 4
+                    if (len(PointCodes.objects.filter(catagory=p[0].text).filter(
+                            code=int(p[1].text))) == 0):
+                        type = PointCodes(catagory=p[0].text, code=int(p[1].text),
+                                          description=str(p[0].text) + str(p[1].text))
+                        type.save()
+                    else:
+                        type = PointCodes.objects.filter(catagory=p[0].text).get(code=int(p[1].text))
+
+                    g_obj.points_set.create(type=type, amount=float(p[2].text), )
+            # print(f"added student {int(s[0].text)}")
+        except Exception as e:
+            student_num = int(s[0].text)
+            print(f"Failed to add student {int(s[0].text)}")
+            logs.append(f"Failed to add student {int(s[0].text)}")
+
+            # delete the partially formed student
+            if len(Student.objects.filter(student_num=student_num)) != 0:
+                Student.objects.get(student_num=student_num).delete()
+
+    for plist in root[1]:
+        print(plist)
+
+
+def ajax_import_status(request):
+    global logs
+    data = {
+        'logs': logs,
+    }
+    return JsonResponse(data)
--- data/views.py
+++ data/views.py
@@ -12,8 +12,10 @@ import xml.dom.minidom as minidom
 from util.queryParse import parseQuery
 from django.contrib.auth.decorators import login_required
 from util.converter import wdb_convert
+from threading import Thread
 
 from .ajax_views import *
+logs = []
 
 
 @login_required
@@ -154,73 +156,18 @@ def archive(request):
 
 
 def archive_submit(request):
+    global logs
     logs = []
     if request.method == "POST":
         if request.user.has_perm('data.add_student'):
             if "file" in request.FILES:
-                tree = ET.parse(request.FILES["file"])
-                root = tree.getroot()
-
-                # all students
-                for s in root[0]:
-                    try:
-                        if len(Student.objects.filter(student_num=int(s[0].text))) != 0:
-                            print(f"student with number {s[0].text} already exists")
-                            logs.append(f"student with number {s[0].text} \t ({s[4].text}, {s[3].text}) already exists")
-                            continue
-
-                        s_obj = Student(
-                            student_num=int(s[0].text),
-                            homeroom=f"{s[1].text.zfill(2)}{s[2].text}",
-                            first=s[3].text,
-                            last=s[4].text,
-                            legal=s[5].text,
-                            sex=s[6].text,
-                            grad_year=int(s[7].text)
-                        )
-                        s_obj.save()
-
-                        for g in s[8]:
-
-                            g_obj = s_obj.grade_set.get(grade=int(g[0].text))
-                            g_obj.anecdote = g[2].text or ""
-
-                            scholar = g_obj.scholar_set.all().first()
-
-                            scholar.term1 = float(g[3].text)
-                            scholar.term2 = float(g[4].text)
-                            scholar.save()
-
-                            g_obj.save()
-
-                            for p in g[5]:  # fix so that the codes are the last 2 digits instead of last 4
-                                if (len(PointCodes.objects.filter(catagory=p[0].text).filter(
-                                        code=int(p[1].text))) == 0):
-                                    type = PointCodes(catagory=p[0].text, code=int(p[1].text),
-                                                      description=str(p[0].text) + str(p[1].text))
-                                    type.save()
-                                else:
-                                    type = PointCodes.objects.filter(catagory=p[0].text).get(code=int(p[1].text))
-
-                                g_obj.points_set.create(type=type, amount=float(p[2].text), )
-                        # print(f"added student {int(s[0].text)}")
-                    except Exception as e:
-                        student_num = int(s[0].text)
-                        print(f"Failed to add student {int(s[0].text)}")
-                        logs.append(f"Failed to add student {int(s[0].text)}")
-
-                        # delete the partially formed student
-                        if len(Student.objects.filter(student_num=student_num)) != 0:
-                            Student.objects.get(student_num=student_num).delete()
-
-                for plist in root[1]:
-                    print(plist)
+                file = ET.parse(request.FILES["file"])
+                import_thread = Thread(target=import_pgdb_file, args=(file, ))
+                import_thread.start()
+                logs.append("We will now import the file in the background")
         else:
             logs.append("Permission error: Please make sure you can import students")
-
-    template = get_template('data/archive.html')
-    if not logs:
-        logs.append("Import Successful")
+    template = get_template('data/file_upload.html')
     context = {
         "logs": logs,
     }
