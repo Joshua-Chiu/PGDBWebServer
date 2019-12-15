801c5052b5baacc9b05c3d189e6c075e1cd0391e
Joshua Chiu <joshuachiu2020@gmail.com>
2019-12-15 01:53:12 -0800

    more UI changes

--- PGDBWebServer/settings.py
+++ PGDBWebServer/settings.py
@@ -90,7 +90,7 @@ WSGI_APPLICATION = 'PGDBWebServer.wsgi.application'
 #         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
 #     }
 # }
-'''
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
@@ -112,7 +112,7 @@ DATABASES = {
         'PORT': '3306'
     }
 }
-
+'''
 
 # Password validation
 # https://docs.djangoproject.com/en/2.0/ref/settings/#auth-password-validators
