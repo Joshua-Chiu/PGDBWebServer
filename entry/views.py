f5769a9314b0796ac7908ae2f91cebb56830302b
Joshua Chiu <1156449@learn.vsb.bc.ca>
2020-04-23 16:52:09 -0700

    added entries for check

--- entry/views.py
+++ entry/views.py
@@ -348,6 +348,16 @@ def check_file(request, point_catagory):
                         f"({student.student_num}) was not entered: POINTS EXCEEDED MAXIMUM VALUE OF 10")
                     continue
 
+                try:
+                    if point_catagory == "SE":
+                        error_msgs.append(
+                            f"Success: {student.first} {student.last} ({student.student_num}): {minutes} hours {points} point(s) in  {point_catagory}{code} {point_type.description}")
+                    else:
+                        error_msgs.append(
+                            f"Success: {student.first} {student.last} ({student.student_num}): {points} point(s) in  {point_catagory}{code} {point_type.description}")
+                except:
+                    error_msgs.append("General Error Raised")
+
     template = get_template('entry/submission-summary.html')
     context = {
         'usage': "check",
